6897d426a72e2655d940b890bf4f9e6052f20aa2
unknown <santifp1997@gmail.com>
2023-07-26 23:57:03 -0300

    store

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -21,14 +21,31 @@ const Cart: React.FC<{ onClose: () => void }> = (props) => {
             alt="delete"
           />
         </div>
-        <p className="text-center font-semibold w-11/12 text-lg">
+        <p className="text-center font-semibold w-11/12 pl-10 text-lg  lg:pl-0">
           {!cart.length ? "CARRO VACÍO" : "TU CARRITO"}
         </p>
       </div>
 
-      <div className="flex flex-row">
+      <div>
+        <div
+          className={`w-full text-center`}
+        >
+          {cart.length == 1 ? (
+            <div>
+              <span>Tienes </span>
+              <span className="itemsNumber">{cart.length}</span>
+              <span>Item </span>
+            </div>
+          ) : (
+            <div>
+              <span>Tienes </span>
+              <span className="itemsNumber">{cart.length}</span>
+              <span>Items </span>
+            </div>
+          )}
+        </div>
         {cart.length > 0 && (
-          <div className="text-xs w-[30%] lg:hidden">
+          <div className="text-xs w-[30%] -mt-8 lg:hidden">
             <ul>
               <li key="n">N. - Nombre</li>
               <li key="c">C. - Cantidad</li>
@@ -36,22 +53,6 @@ const Cart: React.FC<{ onClose: () => void }> = (props) => {
             </ul>
           </div>
         )}
-
-        <div className={`text-center w-[32%] ${cart.length === 0 && 'w-full'} lg:w-full`}>
-          {cart.length == 1 ? (
-            <div>
-              <span>Tienes </span>
-              <span className="itemsNumber">{cart.length}</span>
-              Item
-            </div>
-          ) : (
-            <div>
-              <span>Tienes </span>
-              <span className="itemsNumber">{cart.length}</span>
-              Items
-            </div>
-          )}
-        </div>
       </div>
 
       {cart.length > 0 && (
@@ -66,8 +67,8 @@ const Cart: React.FC<{ onClose: () => void }> = (props) => {
           </div>
 
           <div className="cartDiv text-2xl lg:hidden  ">
-            <p className=" w-[60%]">N.</p>
-            <div className="flex flex-row w-[40%]">
+            <p className=" w-[45%]">N.</p>
+            <div className="flex flex-row w-[55%]">
               <p className=" w-1/3 ">C.</p>
               <p className=" w-1/3  ">T.</p>
               <div className="w-1/3 "></div>
@@ -79,8 +80,8 @@ const Cart: React.FC<{ onClose: () => void }> = (props) => {
       {cart.map((el) => {
         return (
           <div key={el.id} className="cartDiv">
-            <p className="w-[60%] lg:w-[75%]">{el.name}</p>
-            <div className="flex flex-row w-[40%] lg:w-[25%]">
+            <p className="w-[45%] lg:w-[75%]">{el.name}</p>
+            <div className="flex flex-row w-[55%] lg:w-[25%]">
               <p className=" w-1/3 lg:w-[42%] text-right pr-5">X{el.amount}</p>
               <p className=" w-1/3 lg:w-[42%]">${el.price}</p>
               <div className=" w-1/3 flex  justify-end lg:justify-center lg:w-[16%] ">
