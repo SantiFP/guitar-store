cbe7846050263ec5aae96788305ce6b5572b4c23
unknown <santifp1997@gmail.com>
2023-08-11 01:32:53 -0300

    store update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import Item from "@/components/Item/Item";
 import fs from "fs";
 import path from "path";
 import { ItemObj } from "@/Types/types";
-// import https from "https";
+import https from "https";
 
 export default function Home(props: { data: ItemObj[] }) {
-  const { data } = props;
+  // const { data } = props;
+  console.log(props);
   return (
     <>
-      <div className="lg:flex lg:flex-row lg:w-11/12 lg:mx-auto lg:flex-wrap">
+      {/* <div className="lg:flex lg:flex-row lg:w-11/12 lg:mx-auto lg:flex-wrap">
         {data.map((el, i) => (
           <div
             className={`lg:w-1/3 lg:mt-5 ${i === data.length - 1 && "pb-6"}`}
@@ -24,47 +25,49 @@ export default function Home(props: { data: ItemObj[] }) {
             />
           </div>
         ))}
-      </div>
+      </div> */}
     </>
   );
 }
 
 export async function getStaticProps() {
+
   try {
-    const filePath = path.join(process.cwd(), "guitarsApi", "guitarsApi.json");
+    // const filePath = path.join(process.cwd(), "guitarsApi", "guitarsApi.json");
 
-    const fileContent = await fs.promises.readFile(filePath, "utf8");
+    // const fileContent = await fs.promises.readFile(filePath, "utf8");
 
-    const data = JSON.parse(fileContent);
+    // const data = JSON.parse(fileContent);
 
-    return {
-      props: {
-        data,
-      },
-    };
+    // return {
+    //   props: {
+    //     data,
+    //   },
+    // };
 
     //////////////////////////// FETCH A URL /////////////////////////////////////////////////
 
-    // const url = "https://jsonplaceholder.typicode.com/posts";
+    const url = "https://jsonplaceholder.typicode.com/posts";
 
-    // return new Promise((resolve, reject) => {
-    //   https
-    //     .get(url, (response) => {
-    //       let data = "";
+    return new Promise((resolve, reject) => {
+      https
+        .get(url, (response) => {
+          let data = "";
 
-    //       response.on("data", (chunk) => {
-    //         data += chunk;
-    //       });
+          response.on("data", (chunk) => {
+            data += chunk;
+            console.log(chunk);
+          });
 
-    //       response.on("end", () => {
-    //         const parsedData = JSON.parse(data);
-    //         resolve({ props: { data: parsedData } });
-    //       });
-    //     })
-    //     .on("error", (error) => {
-    //       reject(error);
-    //     });
-    // });
+          response.on("end", () => {
+            const parsedData = JSON.parse(data);
+            resolve({ props: { data: parsedData } });
+          });
+        })
+        .on("error", (error) => {
+          reject(error);
+        });
+    });
 
   } catch (error) {
     console.error("Error fetching data");
